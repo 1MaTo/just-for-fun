104fc8bd918433fef49b865a08757dcc5e4058e4
MaTo <ily4368@gmail.com>
2020-07-12 20:15:31 +0300

    add background IMG and MP3 to project
    Also add styling to bg image

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import css from "../styles/index.module.scss"
+import { useEffect } from "react"
 
 export default function HomePage() {
 
-    return <div className={css.text}>Test</div>
+    useEffect(() => {
+        var audio = new Audio("/sparkle.mp3")
+        audio.play();
+    },[])
+
+    return <div className={css.background}>
+        <div className={css.backgroundImage}>
+            <div className={css.text}>Test</div>
+        </div>
+    </div>
 
 }
