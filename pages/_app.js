122387d8a86b2eae9ca823f9bc784627054ec8e7
MaTo <ily4368@gmail.com>
2020-07-12 15:24:53 +0300

    add sass to project

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/global.scss'
+
+export default function JustForFunApp({ Component, pageProps }) {
+
+    return <Component {...pageProps} />
+
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import css from "../styles/index.module.scss"
+
 export default function HomePage() {
 
-    return <div>Just start</div>
+    return <div className={css.text}>Test</div>
 
 }
